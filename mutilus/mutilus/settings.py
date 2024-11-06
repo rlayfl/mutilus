e3b6739140f8f40f2d09df2f4f792935fce759f5
richardpearceopoord <richardpearce@opord.co.uk>
2024-11-06 21:52:58 +0000

    Added STATICFILES_DIRS to settings.py to allow static files to work

--- mutilus/mutilus/settings.py
+++ mutilus/mutilus/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#Add this in your settings.py file:
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
