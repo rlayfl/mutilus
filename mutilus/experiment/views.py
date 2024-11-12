daca279671fd73dcf594bfdb8748e33a92201b09
richardpearceopoord <richardpearce@opord.co.uk>
2024-11-12 20:26:57 +0000

    Added comment skeleton code to views

--- mutilus/experiment/views.py
+++ mutilus/experiment/views.py
@@ -2,6 +2,17 @@ from django.http import HttpResponse
 from django.template import loader
 
 def experiment(request, experimentNumber):
+
+    # Get the experiment information from firebase
+
+    # Decide the list of images which are going to be shown
+
+    # For each image, show some options to click
+
+    # Store the result
+    
+
+
     # Load the experiment template
     template = loader.get_template('experiment.html')
     # Context with experimentNumber
