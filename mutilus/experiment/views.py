cbca1e1453d6283ef6b907652a2848390c47842d
richardpearceopoord <richardpearce@opord.co.uk>
2024-11-12 20:18:47 +0000

    Updated urls and views to take GET request numbers

--- mutilus/experiment/views.py
+++ mutilus/experiment/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
 from django.template import loader
 
-def experiment(request):
+def experiment(request, experimentNumber):
+    # Load the experiment template
     template = loader.get_template('experiment.html')
-    return HttpResponse(template.render())
+    # Context with experimentNumber
+    context = {'experimentNumber': experimentNumber}
+    # Render the template with context
+    return HttpResponse(template.render(context, request))
 
 
-def begin(request):
+def begin(request, experimentNumber):
+    # Load the begin template
     template = loader.get_template('begin.html')
-    return HttpResponse(template.render())
+    # Context with experimentNumber
+    context = {'experimentNumber': experimentNumber}
+    # Render the template with context
+    return HttpResponse(template.render(context, request))
