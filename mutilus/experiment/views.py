4666152ed41835468c9cb8a2554f0594d1595589
rlayfl <rlayfl@essex.ac.uk>
2025-02-04 17:39:37 +0000

    Added some time elapsed changes

--- mutilus/experiment/views.py
+++ mutilus/experiment/views.py
@@ -65,12 +65,14 @@ def upload_experiment_answer_to_firebase(formData):
     uid = formData.POST['uid']
     answer = formData.POST['answer']
     realAnswer = formData.POST['realAnswer']
+    timeElapsed = formData.POST['timeElapsed']
 
-    URL = 'https://mutilus-7d3b1-default-rtdb.europe-west1.firebasedatabase.app/experiments/'+uid+'.json?auth=AIzaSyAQt-LmICWeHMo8tNGDgvh8a0_2OS-nnP0'
+    URL = 'https://mutilus-7d3b1-default-rtdb.europe-west1.firebasedatabase.app/answers/'+uid+'.json?auth=AIzaSyAQt-LmICWeHMo8tNGDgvh8a0_2OS-nnP0'
 
     jsondata = {
         "answer": answer,
-        "realAnswer": realAnswer
+        "realAnswer": realAnswer,
+        "timeElapsed": timeElapsed
     }
      
     response = requests.post(URL, json=jsondata, headers={"Content-Type": "application/json"})
