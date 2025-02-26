663c024c1c85f941796d3b3d15a80c1a27243b56
rlayfl <rlayfl@essex.ac.uk>
2025-02-26 16:04:58 +0000

    Uploaded images and changed structure to allow for multiple buoy types

--- mutilus/experiment/views.py
+++ mutilus/experiment/views.py
@@ -12,47 +12,63 @@ from PIL import Image
 def experiment(request, experimentNumber):
 
     # Define the path to the images directories
-    real_images_path = os.path.join(settings.STATICFILES_DIRS[0], 'images', 'marker_buoys', 'real', 'cardinal_mark_north')
-    synthetic_images_path = os.path.join(settings.STATICFILES_DIRS[0], 'images', 'marker_buoys', 'synthetic', 'cardinal_mark_north')
+    real_images_path = os.path.join(settings.STATICFILES_DIRS[0], 'images', 'marker_buoys', 'real')
+    synthetic_images_path = os.path.join(settings.STATICFILES_DIRS[0], 'images', 'marker_buoys', 'synthetic')
 
-    # Get the list of image files for real and fake images
-    real_image_files = [f for f in os.listdir(real_images_path) if os.path.isfile(os.path.join(real_images_path, f))]
-    synthetic_image_files = [f for f in os.listdir(synthetic_images_path) if os.path.isfile(os.path.join(synthetic_images_path, f))]
+    # Define buoy types and their corresponding directories
+    buoy_types = {
+        'cardinal_mark_north': 1,
+        'cardinal_mark_east': 2,
+        'cardinal_mark_south': 3,
+        'cardinal_mark_west': 4,
+        'special_mark': 5,
+        'isolated_danger_mark': 6,
+        'safe_water_mark': 7,
+        'port_side_mark': 8,
+        'starboard_side_mark': 9
+    }
 
     # Create an array to hold image information
     images_info = []
-    buoy_type = 1
     image_number = 1
 
-    # Loop through the real image files and get their resolution
-    for image_file in real_image_files:
-        image_path = os.path.join(real_images_path, image_file)
-        with Image.open(image_path) as img:
-            width, height = img.size
-            images_info.append({
-                'filename': image_file,
-                'width': width,
-                'height': height,
-                'type': buoy_type,
-                'is_synthetic': False,
-                'image_number': image_number
-            })
-            image_number += 1
+    # Loop through each buoy type
+    for buoy_type, type_id in buoy_types.items():
+        # Get the list of image files for real and synthetic images
+        real_image_files = [f for f in os.listdir(os.path.join(real_images_path, buoy_type)) if os.path.isfile(os.path.join(real_images_path, buoy_type, f))]
+        synthetic_image_files = [f for f in os.listdir(os.path.join(synthetic_images_path, buoy_type)) if os.path.isfile(os.path.join(synthetic_images_path, buoy_type, f))]
 
-    # Loop through the synthetic image files and get their resolution
-    for image_file in synthetic_image_files:
-        image_path = os.path.join(synthetic_images_path, image_file)
-        with Image.open(image_path) as img:
-            width, height = img.size
-            images_info.append({
-                'filename': image_file,
-                'width': width,
-                'height': height,
-                'type': buoy_type,
-                'is_synthetic': True,
-                'image_number': image_number
-            })
-            image_number += 1
+        # Loop through the real image files and get their resolution
+        for image_file in real_image_files:
+            image_path = os.path.join(real_images_path, buoy_type, image_file)
+            with Image.open(image_path) as img:
+                width, height = img.size
+                images_info.append({
+                    'filename': image_file,
+                    'width': width,
+                    'height': height,
+                    'type': buoy_type,
+                    'typeID': type_id,
+                    'is_synthetic': False,
+                    'image_number': image_number
+                })
+                image_number += 1
+
+        # Loop through the synthetic image files and get their resolution
+        for image_file in synthetic_image_files:
+            image_path = os.path.join(synthetic_images_path, buoy_type, image_file)
+            with Image.open(image_path) as img:
+                width, height = img.size
+                images_info.append({
+                    'filename': image_file,
+                    'width': width,
+                    'height': height,
+                    'type': buoy_type,
+                    'typeID': type_id,
+                    'is_synthetic': True,
+                    'image_number': image_number
+                })
+                image_number += 1
 
     # Shuffle the order of images_info
     random.shuffle(images_info)
@@ -63,9 +79,9 @@ def experiment(request, experimentNumber):
         {'id': 'cme', 'data_value': 2, 'text': 'Cardinal Mark East'},
         {'id': 'cms', 'data_value': 3, 'text': 'Cardinal Mark South'},
         {'id': 'cmw', 'data_value': 4, 'text': 'Cardinal Mark West'},
-        {'id': 'smc2s', 'data_value': 5, 'text': 'Special Mark Class 2 Solar'},
+        {'id': 'sm', 'data_value': 5, 'text': 'Special Mark Class 2 Solar'},
         {'id': 'idm', 'data_value': 6, 'text': 'Isolated Danger Mark'},
-        {'id': 'swmt2', 'data_value': 7, 'text': 'Safe Water Mark Type 2'},
+        {'id': 'swm', 'data_value': 7, 'text': 'Safe Water Mark Type 2'},
         {'id': 'psm', 'data_value': 8, 'text': 'Port (Left) Side Marker'},
         {'id': 'ssm', 'data_value': 9, 'text': 'Starboard (Right) Side Marker'}
     ]
